f2e22ce2ed1f36e63d76a75f933f034afcae6fa1
Gustavo Calca <gstv.calca@gmail.com>
2025-03-31 18:04:40 +1100

    feat: added theme toggle

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 const openModalButton = document.querySelectorAll('.open_modal_button');
 const closeModalButton = document.querySelectorAll('.close_modal_button');
 const modal = document.querySelectorAll('.warning_modal_container');
+const toggleTheme = document.querySelector('#toggle_theme');
+const rootElement = document.documentElement;
+const defaultTheme = getComputedStyle(rootElement).getPropertyValue('--bg-primary');
+const lightTheme = "#ffffff";
+
 
 /* Add the onClick event on each button to open the modal */ 
 openModalButton.forEach(btn => {
@@ -37,3 +42,28 @@ modal.forEach(md => {
     }
   })
 })
+
+toggleTheme.addEventListener('click', () => {
+  if(getComputedStyle(rootElement).getPropertyValue('--bg-primary') === defaultTheme){
+    rootElement.style.setProperty('--text-primary', 'rgb(50, 50, 50)');
+    rootElement.style.setProperty('--text-secondary', 'rgb(100, 100, 100)');
+    rootElement.style.setProperty('--bg-primary', 'rgb(250, 250, 250)');
+    rootElement.style.setProperty('--bg-secondary', 'rgb(240, 240, 240)');
+    rootElement.style.setProperty('--border-color', 'rgb(50, 50, 50)');
+    rootElement.style.setProperty('--bg-gradient-start', '#464cf6');
+    rootElement.style.setProperty('--bg-gradient-ent', '#77ccf4');
+    rootElement.style.setProperty('--bg-modal', 'rgba(0, 0, 0, 0.2)');
+  }
+  else {
+    rootElement.style.setProperty('--text-primary', 'white');
+    rootElement.style.setProperty('--text-secondary', '#b3b3b3');
+    rootElement.style.setProperty('--bg-primary', '#2c2c2c');
+    rootElement.style.setProperty('--bg-secondary', 'rgb(60, 60, 60)');
+    rootElement.style.setProperty('--border-color', 'white');
+    rootElement.style.setProperty('--bg-gradient-start', '#464cf6');
+    rootElement.style.setProperty('--bg-gradient-ent', '#77ccf4');
+    rootElement.style.setProperty('--bg-modal', 'rgba(0, 0, 0, 0.8)');
+  }
+})
+
+
